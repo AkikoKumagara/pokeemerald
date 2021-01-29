6a461ce12dd11fa12b98bb88c687e26621b7d103
DENERKA-DESKTOP\Denerka <coreydenoncourt@gmail.com>
2021-01-28 20:31:26 -0500

    Reusable TMs | Run indoors | Change Birch intro pokemon | Gfx fix pokecenter healing eggs | Eggs hatch lvl 1 | Gfx fix surfing dismount | Forget HMs and any moves | Nature-based stat colors summary screen | Fishing mechanics changed to FRLG |

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1661,6 +1661,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         ScriptContext2_Disable();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+		playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
@@ -1784,15 +1785,6 @@ static bool8 Fishing_ShowDots(struct Task *task)
 
     AlignFishingAnimationFrames();
     task->tFrameCounter++;
-    if (JOY_NEW(A_BUTTON))
-    {
-        task->tStep = FISHING_NO_BITE;
-        if (task->tRoundsPlayed != 0)
-            task->tStep = FISHING_GOT_AWAY;
-        return TRUE;
-    }
-    else
-    {
         if (task->tFrameCounter >= 20)
         {
             task->tFrameCounter = 0;
@@ -1810,7 +1802,6 @@ static bool8 Fishing_ShowDots(struct Task *task)
             }
         }
         return FALSE;
-    }
 }
 
 static bool8 Fishing_CheckForBite(struct Task *task)
@@ -1853,10 +1844,7 @@ static bool8 Fishing_CheckForBite(struct Task *task)
 
 static bool8 Fishing_GotBite(struct Task *task)
 {
-    AlignFishingAnimationFrames();
-    AddTextPrinterParameterized(0, 1, gText_OhABite, 0, 17, 0, NULL);
-    task->tStep++;
-    task->tFrameCounter = 0;
+    task->tStep += 3;
     return FALSE;
 }
 
